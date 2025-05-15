ccfc2c47c741586210f3639b8b1c363861eb5fb5
johnnykoo84 <johnnykoo84@gmail.com>
2025-05-15 13:21:59 +0900

    Fix markdown blog display

--- svelte.config.js
+++ svelte.config.js
@@ -27,7 +27,8 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
 		alias: {
-			'@': 'src/lib'
+			'@': 'src/lib',
+			'@blog': 'src/routes/(landing)/blog'
 		}
 	}
 };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -58,7 +58,8 @@ const config: Config = {
 				sans: [...fontFamily.sans]
 			}
 		}
-	}
+	},
+	plugins: [require('@tailwindcss/typography')]
 };
 
 export default config;
